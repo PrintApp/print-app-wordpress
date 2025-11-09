39a55b3d1b54e0a82378f8ea883634fc9c010a32
Isaac Coker <81635623+iocoker@users.noreply.github.com>
2025-11-09 19:12:32 +0100

    Add localization for print app script parameters

--- functions/front/accounts.php
+++ functions/front/accounts.php
@@ -18,7 +18,12 @@
 
 		$run_url = PRINT_APP_RUN_BASE_URL . "user/{$printapp_domain_key}/{$user_id}/wp?signature={$credentials['signature']}&time={$credentials['timestamp']}&lang={$lang_code}";
 		wp_enqueue_script('print_app_class', $run_url, '', '', true);
+        
+		wp_localize_script('print_app_class', 'printAppParams', array(
+			'pluginRoot' 	=> site_url() . '/print_app',
+			'cookieKey' 	=> PRINT_APP_CUSTOMIZATION_KEY,
+			'wp_ajax_url' 	=> admin_url('admin-ajax.php'),
+		));
 
         echo '<div id="print-app-user-projects" class="print-app-user-projects"></div>';
     }
-    
